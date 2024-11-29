1894af3402d2e6dd2704c265b4675915b9de8354
otaviosrb <150717463+otaviosrb@users.noreply.github.com>
2024-11-29 15:26:54 -0300

    Adicionei máximo de 6 tentativas, letras erradas, upgrade no design

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 import os
 from random import choice
 
-palavras = ["computador", "programador", "desafio", "tecnologia", "algoritmo",
-            "inteligencia", "python", "aprendizado", "internet", "desenvolvimento"]
+palavras = ["COMPUTADOR", "PROGRAMADOR", "DESAFIO", "TECNOLOGIA", "ALGORITMO",
+ "INTELIGENCIA", "PYTHON", "APRENDIZADO", "INTERNET", "DESENVOLVIMENTO"]
 PALAVRA = choice(palavras)
-tentativas = 0
+tentativas = 6
 palavra_codificada = '*' * len(PALAVRA)
+letras_erradas = []
+letras_certas = []
 
 print(' JOGO DA FORCA '.center(50, '='))
-print()
-while palavra_codificada != PALAVRA:
-    letra = input ('Digite uma letra: ').lower()
+while tentativas > 0:
+    print()
+    print(f'Tentativas: {tentativas}')
+    print(f'Letras erradas: {", ".join(letras_erradas)}')
+    letra = input ('Digite uma letra: ').upper()
+    os.system('cls')
+    if letra == '' or letra == ' ':
+        print('Digite algo!')
+        print(palavra_codificada)
+        continue
     validacao_alfa = letra.isalpha()
-    tentativas += 1
     if len(letra) > 1:
         print('Digite apenas UMA letra')
+        print(palavra_codificada)
+        continue
     if validacao_alfa == False:
         print('Digite apenas LETRAS')
+        print(palavra_codificada)
+        continue
+    if letra in letras_certas:
+        print('Esta letra já foi verificada, tente outra!')
+        print(palavra_codificada)
+        continue
     if letra in PALAVRA:
+        letras_certas.append(letra)
         for i in range(len(PALAVRA)):
             if PALAVRA[i] == letra:
                 palavra_codificada = palavra_codificada[:i] + letra + palavra_codificada[i+1:]
         print(palavra_codificada)
+    if letra not in PALAVRA:
+        if letra not in letras_erradas:
+            letras_erradas.append(letra)
+        else:
+            print('Você já tentou essa letra!')
+            print(palavra_codificada)
+            continue
+        tentativas -= 1
+        print(palavra_codificada)
+    if palavra_codificada == PALAVRA:
+        os.system('cls')
+        print('VOCÊ GANHOU, PARABÉNS!!')
+        print(f'A palavra era: {PALAVRA}')
+        exit()
 os.system('cls')
-print('VOCÊ GANHOU, PARABÉNS!!')
-print(f'A palavra era: {PALAVRA}')
-print(f'Tentativas: {tentativas}')
+print('Você perdeu, não foi dessa vez!')
